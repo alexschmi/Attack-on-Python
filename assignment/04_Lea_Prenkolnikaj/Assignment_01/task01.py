76c2f873a7ffe8fac0f5a5a8b0cddd9beff70448
leaprenkolnikaj <118899274+leaprenkolnikaj@users.noreply.github.com>
2022-11-26 17:18:33 +0100

    Task1

---
+++ assignment/04_Lea_Prenkolnikaj/Assignment_01/task01.py
@@ -0,0 +1,5 @@
+#Task 1: Write a program that asks the user to input a name and outputs a simple greeting.
+print("What's your name?")
+NAME = input() #Here the user inputs their name
+print("Hi"+ NAME + "! Nice to meet you!")
+print("Welcome to the programming course!")
