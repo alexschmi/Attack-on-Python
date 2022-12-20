f08b930ff3b6c8499a271991a29470522cabc62a
xNayanK <118897561+xNayanK@users.noreply.github.com>
2022-12-20 19:27:44 +0100

    Update Task02.py

--- assignment/05_NayanKnyrim_Assignment01/assignment03/Task02.py
+++ assignment/05_NayanKnyrim_Assignment01/assignment03/Task02.py
@@ -27,10 +27,52 @@ class Customer():
 
 
 class CustomerSavings(Customer):
-    def __init__(self, name, balance):
+    def __init__(self, name, balance, savings):
         super().__init__(name, balance)
+        self.savings = savings
+
+    def s_tracking(self):
+        Customer.tracking(self)
+        print("savings: ", self.savings)
+
+    def transfer_main2sav(self, amount):
+        if amount < 0:
+            print(">>cannot transfer negative amount<<")
+        elif self.balance - amount < 0:
+            print(">>insufficient balance<<")
+        self.balance = self.balance - amount
+        self.savings = self.savings + amount
+
+    def transfer_sav2main(self, amount):
+        if amount < 0:
+            print(">>cannot transfer negative amount<<")
+        elif self.savings - amount < 0:
+            print(">>insufficient savings<<")
+        else:
+            self.balance = self.balance + amount
+            self.savings = self.savings - amount
+
 
 bob = Customer('bob', 0)
 print(bob.tracking())
 bob.deposit(100)
 bob.withdraw(50)
+print("")
+
+bob = CustomerSavings(bob.name, bob.balance, 50)
+print(bob.s_tracking())
+
+print("")
+
+bob.transfer_main2sav(50)
+print(bob.s_tracking())
+
+print("")
+
+bob.transfer_sav2main(100)
+print(bob.s_tracking())
+
+print("")
+
+bob.transfer_main2sav(200)
+
