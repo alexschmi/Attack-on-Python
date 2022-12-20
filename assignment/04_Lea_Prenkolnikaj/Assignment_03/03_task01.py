b380abc571cffd5b6fe80330f586de008b7dd9c0
leaprenkolnikaj <118899274+leaprenkolnikaj@users.noreply.github.com>
2022-12-20 21:32:40 +0100

    Add files via upload

---
+++ assignment/04_Lea_Prenkolnikaj/Assignment_03/03_task01.py
@@ -0,0 +1,19 @@
+#Task 01: Dictionary
+
+costumer = ['Nico', 'Kevin', 'Lea']
+
+def build_database():
+    acc = {c: 0 for c in costumer}
+    return acc
+
+print(build_database()) print the dictionary
+
+
+
+
+#Add new customer
+
+costumer = 'Marius"
+acc['Marius] = 0
+
+print(acc)
