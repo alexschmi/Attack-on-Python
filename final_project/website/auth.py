76debb150f0df2d449da7f68e22f055e7b16400b
cafi02 <c.fiedler02@stud.uni-goettingen.de>
2023-01-03 12:54:50 +0100

    prepare sign_up to store users in database

--- final_project/website/auth.py
+++ final_project/website/auth.py
@@ -47,12 +47,12 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
+            # new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+            #     password1, method='sha256'))
+            # db.session.add(new_user)
+            # db.session.commit()
+            # login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+            # return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", user=current_user)
+    return render_template("sign_up.html") # --> return render_template("sign_up.html", user=current_user)
