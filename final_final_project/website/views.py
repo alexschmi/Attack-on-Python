7a6d8fd489483209aeeb5c2e609fd0d770415b59
cafi02 <c.fiedler02@stud.uni-goettingen.de>
2023-01-10 21:07:54 +0100

    connect /help and /flights

--- final_final_project/website/views.py
+++ final_final_project/website/views.py
@@ -7,6 +7,23 @@ def home():
 
     return render_template("home.html")
 
-@views.route('/reservation')
-def res():
-    return render_template("draft.html")
+@views.route('/help')
+def help():
+    return render_template("help.html")
+
+@views.route('/flights', methods=['GET', 'POST'])
+# @login_required
+def reservation():
+#     if request.method == 'POST':
+#         name = request.form.get('name')
+#         phone = request.form.get('phone')
+#         flight = request.form.get('flight')
+#         seat = request.form.get('seat')
+#
+#         if len(name) < 2:
+#             flash('Name must be greater than 1 character.', category='error')
+#         else:
+#             # Insert new User into the database
+#             flash('Reservation completed!', category='success')
+
+    return render_template("flights.html")
