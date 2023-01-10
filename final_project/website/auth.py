87f4f15516b536a6a0e955e937de64c97b1ca8ac
leaprenkolnikaj <lea.prenkolnikaj@stud.uni-goettingen.de>
2023-01-10 12:16:50 +0100

    delete not functioning code

--- final_project/website/auth.py
+++ final_project/website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, logout_user, current_user
-# from . import db
+
+
 
 auth = Blueprint('auth', __name__)
 
@@ -12,23 +12,23 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        # user = User.query.filter_by(email=email).first()
-        if user:
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                # login_user(user, remember=True)                             # User Login
-                return redirect(url_for('views.home'))
-            else:
-                flash('Incorrect password, try again.', category='error')
-        else:
-            flash('Email does not exist.', category='error')
 
-    return render_template("login.html", user=current_user)
+        # if user:
+        #     if check_password_hash(user.password, password):
+        #         flash('Logged in successfully!', category='success')
+        #
+        #         return redirect(url_for('views.home'))
+        #     else:
+        #         flash('Incorrect password, try again.', category='error')
+        # else:
+        #     flash('Email does not exist.', category='error')
+
+    return render_template("login.html")
 
 @auth.route('/logout')
-# @login_required
+
 def logout():
-    # logout_user()
+
     return redirect(url_for('auth.login'))
 
 
@@ -54,6 +54,6 @@ def sign_up():
             # db.session.commit()
             # login_user(user, remember=True)                             # User Login
             flash('Account created!', category='success')
-            # return redirect(url_for('views.home'))
+            return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", user=current_user)
+    return render_template("sign_up.html")
