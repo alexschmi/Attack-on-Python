20fb8e60eb91ad6269a741b8c471313cd349f3b5
cafi02 <c.fiedler02@stud.uni-goettingen.de>
2023-01-11 19:51:03 +0100

    comment classes Flight and User, not needed?

--- final_final_project/website/models.py
+++ final_final_project/website/models.py
@@ -1,18 +1,18 @@
-from . import db
-from flask_login import UserMixin
-from sqlalchemy.sql import func
-
-class Flight(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    seat = db.Column(db.String(10000))
-    passenger = db.Column(db.String(150))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    name = db.Column(db.String(150))
-    password = db.Column(db.String(150))
-    flight = db.relationship('Flight')
+# from . import db
+# from flask_login import UserMixin
+# from sqlalchemy.sql import func
+#
+# class Flight(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     seat = db.Column(db.String(10000))
+#     passenger = db.Column(db.String(150))
+#     date = db.Column(db.DateTime(timezone=True), default=func.now())
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(150), unique=True)
+#     name = db.Column(db.String(150))
+#     password = db.Column(db.String(150))
+#     flight = db.relationship('Flight')
 
